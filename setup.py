bc3a682e613b0539d3d4d6e53d08ec8f6f062d10
Frank Henard <frank@mirusresearch.com>
2018-02-14 12:40:19 -0600

    renaming things

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='image_utils',
+    name='mirus_image_utils',
     version='1.0.1',
     description='Mirus Image Utilities',
     author='Frank Henard',
     author_email='frank@mirusresearch.com',
-    packages=['image_utils'],
+    packages=find_packages(),
     url='https://github.com/mirusresearch/image_utils',
     license='MIT license, see LICENSE.txt',
     install_requires=[
